16b7b5aa1b7f1f831cf40e47c2f69796b6d605b4
leshista <a.leshista@gmail.com>
2021-07-13 17:21:36 +0300

    Commenting all around the way

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Making clicking on switcher reveal block
 let languageSelectionSwitcher = document.querySelector('.language-selection__switcher');
 let languageSelectionBlock = document.querySelector('.language-selection__block');
 languageSelectionSwitcher.addEventListener('click', function () {
