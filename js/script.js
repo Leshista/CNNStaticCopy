2cc2b3afcb038f3c8b265883729a93b7962b06ed
leshista <a.leshista@gmail.com>
2021-07-14 14:56:03 +0300

    Animated the search button & made search do smth

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
-// Making clicking on switcher reveal block, the first way
 let languageSelectionSwitcher = document.querySelector('.language-selection__switcher');
 let languageSelectionBlock = document.querySelector('.language-selection__block');
+let searchBar = document.querySelector('.search__searchbar');
+let searchSubmit = document.querySelector('.search__submit');
+
+
+// Making clicking on switcher reveal block, the first way
 languageSelectionSwitcher.addEventListener('click', function () {
     languageSelectionBlock.classList.toggle('hidden');
+});
+
+// Hitting 'Enter' in submit equals clicking the submit button for some reason
+searchSubmit.addEventListener('click', function () {
+    alert(searchBar.value);
 });
